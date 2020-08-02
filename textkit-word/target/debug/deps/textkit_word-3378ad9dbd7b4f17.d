2df05a91d7fe5faedc906f76460649f9b04e2c9e
Jevgeni Tarasov <jevgeni@hey.com>
2020-08-02 11:36:25 +0200

    first commit

---
+++ textkit-word/target/debug/deps/textkit_word-3378ad9dbd7b4f17.d
@@ -0,0 +1,5 @@
+a:\Dev\textkit\textkit-word\target\debug\deps\textkit_word-3378ad9dbd7b4f17.rmeta: src\lib.rs
+
+a:\Dev\textkit\textkit-word\target\debug\deps\textkit_word-3378ad9dbd7b4f17.d: src\lib.rs
+
+src\lib.rs:
---
+++ textkit-word/target/debug/deps/textkit_word-dbfcc352a91a4783.d
@@ -0,0 +1,5 @@
+a:\Dev\textkit\textkit-word\target\debug\deps\textkit_word-dbfcc352a91a4783.rmeta: src\lib.rs
+
+a:\Dev\textkit\textkit-word\target\debug\deps\textkit_word-dbfcc352a91a4783.d: src\lib.rs
+
+src\lib.rs:
