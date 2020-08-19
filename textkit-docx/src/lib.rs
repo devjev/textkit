4397e1a41162fbc722efd5c63c08486e1fc8d57d
Jevgeni Tarasov <jevgeni@hey.com>
2020-08-19 21:34:45 +0200

    Fix regression from previous fix

--- textkit-docx/src/lib.rs
+++ textkit-docx/src/lib.rs
@@ -21,6 +21,8 @@ static NS_WP_ML: &str = "http://schemas.openxmlformats.org/wordprocessingml/2006
 static PAT_HB_ALL: &str = r"\{\{(\S+)\s*([^\{\}]+)?\}\}"; // All placeholders
 static PAT_HB_SMP: &str = r"\{\{\S+\}\}"; // Only simple placeholders
 static PAT_HB_CPX: &str = r"\{\{[^#/](\S+)\s+([^\{\}]+)?\}\}"; // Only placeholders with helpers
+static PAT_HB_MLS: &str = r"\{\{#(.+)\}\}";
+static PAT_HB_MLE: &str = r"\{\{\\/(.+)\}\}";
 
 type DocxPayload = ZipArchive<Cursor<Vec<u8>>>;
 
@@ -91,35 +93,41 @@ pub(crate) enum TemplateParserFsm {
 
 impl TemplateParserFsm {
     pub fn accumulate(&mut self, text: &str) {
-        let start_uncertain = Regex::new(r"\{").unwrap();
+        let start_uncertain = Regex::new(r"\{$").unwrap();
         let start_simple = Regex::new(r"\{\{").unwrap();
         let start_multi = Regex::new(r"\{\{#").unwrap();
-        let terminating_pattern_multi = Regex::new(r"\{\{/").unwrap();
-        let terminating_pattern_simple = Regex::new(r"\}\}").unwrap();
+        let end_multi = Regex::new(r"\{\{/").unwrap();
+        let end_simple = Regex::new(r"\}\}").unwrap();
 
-        let match_start = |s: &str| {
-            if start_multi.is_match(s) {
-                Self::Accumulating {
-                    acc: String::from(s),
-                    acc_state: TemplateParserFsmAccState::Multiline { depth: 0 },
-                }
-            } else if start_simple.is_match(s) {
-                Self::Accumulating {
-                    acc: String::from(s),
-                    acc_state: TemplateParserFsmAccState::Simple,
-                }
-            } else if start_uncertain.is_match(s) {
-                Self::Accumulating {
-                    acc: String::from(s),
-                    acc_state: TemplateParserFsmAccState::Uncertain,
-                }
-            } else {
-                Self::Done(String::from(s))
-            }
-        };
+        let simple_ph_well_formed = Regex::new(PAT_HB_SMP).unwrap();
+        let multi_ph_start_well_formed = Regex::new(PAT_HB_MLS).unwrap();
+        let multi_ph_end_well_formed = Regex::new(PAT_HB_MLE).unwrap();
 
         *self = match self {
-            Self::Idle => match_start(text),
+            Self::Idle => {
+                // Here we need to decide which case we are handling:
+                // 1. Is it multiline/directive placeholder (well-formed, start or end)?
+                // 2. Is it simple placeholder (well-formed)?
+                // 3. Is it a malformed placeholder?
+                // 4. Is it nothing of the sort?
+                if multi_ph_start_well_formed.is_match(text) {
+                    Self::Accumulating {
+                        acc: text.into(),
+                        acc_state: TemplateParserFsmAccState::Multiline { depth: 0 },
+                    }
+                } else if multi_ph_end_well_formed.is_match(text) {
+                    Self::Done(text.into())
+                } else if simple_ph_well_formed.is_match(text) {
+                    Self::Done(text.into())
+                } else if start_uncertain.is_match(text) {
+                    Self::Accumulating {
+                        acc: text.into(),
+                        acc_state: TemplateParserFsmAccState::Uncertain,
+                    }
+                } else {
+                    Self::Done(text.into())
+                }
+            }
             Self::Accumulating {
                 acc,
                 acc_state: TemplateParserFsmAccState::Multiline { .. },
@@ -127,7 +135,7 @@ impl TemplateParserFsm {
                 let mut new_str = acc.clone();
                 new_str.push_str(text);
                 let opening_brackets = start_multi.find_iter(&new_str).count();
-                let closing_brackets = terminating_pattern_multi.find_iter(&new_str).count();
+                let closing_brackets = end_multi.find_iter(&new_str).count();
                 let still_open = opening_brackets - closing_brackets;
 
                 if still_open > 0 {
@@ -151,7 +159,8 @@ impl TemplateParserFsm {
             } => {
                 let mut new_str = acc.clone();
                 new_str.push_str(text);
-                if terminating_pattern_simple.is_match(&new_str) {
+                // Terminating case should be working here!!!
+                if end_simple.is_match(&new_str) {
                     Self::Done(new_str)
                 } else {
                     Self::Accumulating {
@@ -164,9 +173,34 @@ impl TemplateParserFsm {
                 acc,
                 acc_state: TemplateParserFsmAccState::Uncertain,
             } => {
+                // 1. Check if it's an incomplete multiline placeholder
+                // 2. Check if it's a simple placeholder
+                // 3. Check if it's a finished multiline placeholder
+                // 4. Check if it's a finished simple placeholder
+                // 5. Nothing
                 let mut new_str = acc.clone();
                 new_str.push_str(text);
-                match_start(&new_str)
+
+                // Multi-line, complete
+                if multi_ph_start_well_formed.is_match(&new_str)
+                    && multi_ph_end_well_formed.is_match(&new_str)
+                {
+                    Self::Done(new_str)
+                } else if start_multi.is_match(&new_str) {
+                    Self::Accumulating {
+                        acc: new_str,
+                        acc_state: TemplateParserFsmAccState::Multiline { depth: 0 },
+                    }
+                } else if simple_ph_well_formed.is_match(&new_str) {
+                    Self::Done(new_str)
+                } else if start_simple.is_match(&new_str) {
+                    Self::Accumulating {
+                        acc: new_str,
+                        acc_state: TemplateParserFsmAccState::Simple,
+                    }
+                } else {
+                    Self::Done(new_str.into())
+                }
             }
             Self::Done(x) => Self::Done(x.clone()),
         }
